fc408143adb5e738642a88413884705757d1b447
izuharaaa <izuhara@ritaworks.jp>
2020-05-20 22:38:28 +0900

    OGP修正（表示なし）

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,14 +67,6 @@ module.exports = {
         icon: `src/images/common/emond-icon.png`,
       },
     },
-    {
-      resolve: `@akr4/gatsby-plugin-og-image`,
-      options: {
-        siteUrl: `https://emond.jp`,
-        render: renderOgImage,
-        concurrency: 10,
-      }
-    },
     `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline

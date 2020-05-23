0a6c8d82ae43da90968931cea5d2c8b3cb7b940e
izuharaaa <izuhara@ritaworks.jp>
2020-05-23 11:50:06 +0900

    OGP調整

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = ({ description, lang, meta, image, title }) => {
+const SEO = ({ description, lang, meta, title }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -39,50 +39,22 @@ const SEO = ({ description, lang, meta, image, title }) => {
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
-        {
-          property: `keywords`,
-          content: `emond,笑門道,デザイナー,デザイン,webサービス,アパレル,飲食店,ブランディング,飲食店ブランディング`,
-        },
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `author`,
-          content: `長門 弥`,
-        },
-        {
-          property: `format-detection`,
-          content: `telephone=no`,
-        },
-        {
-          property: `canonical`,
-          content: site.siteMetadata.siteUrl,
-        },
-        {
-          property: `og:locale`,
-          content: "ja_JP",
-        },
-        {
-          property: `og:url`,
-          content: site.siteMetadata.siteUrl,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:image`,
-          content: `/images/common/ogp.png`,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:title', content: 'EMOND' },
+        { name: 'twitter:description', content: metaDescription },
+        { name: 'twitter:image', content: '/images/common/ogp.png' },
+
+        { property: `keywords`, content: `emond,笑門道,デザイナー,デザイン,webサービス,アパレル,飲食店,ブランディング,飲食店ブランディング`,},
+        { name: `description`, content: metaDescription,},
+        { property: `author`, content: `長門 弥`,},
+        { property: `format-detection`, content: `telephone=no`,},
+        { property: `canonical`, content: site.siteMetadata.siteUrl,},
+        { property: `og:locale`, content: "ja_JP",},
+        { property: `og:url`, content: site.siteMetadata.siteUrl,},
+        { property: `og:title`, content: title,},
+        { property: `og:description`, content: metaDescription,},
+        { property: `og:image`, content: `/images/common/ogp.png`,},
+        { property: `og:type`, content: `website`,},
       ].concat(meta)}
     />
   )
